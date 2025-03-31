e40b45048cb34a1ea83e9de6113b248aab9c04b6
Maio Barbero <maiowebdesign@gmail.com>
2025-03-31 10:04:56 +0200

    Fix: fix word selection

--- lua/nvim_wrapper/init.lua
+++ lua/nvim_wrapper/init.lua
@@ -3,8 +3,18 @@ local Wrapper = {}
 function Wrapper.wrap_word(char)
 	local word = vim.fn.expand("<cword>")
 
-	local word_start = vim.fn.col("\\<") - 1
-	local word_end = vim.fn.col("\\>")
+	local row = vim.fn.line(".") - 1 -- 0-indexed
+	local cursor_col = vim.fn.col(".") - 1 -- 0-indexed
+
+	local line = vim.api.nvim_get_current_line()
+
+	local word_start = line:sub(1, cursor_col + 1):match(".*()%w+$")
+	if not word_start then
+		word_start = cursor_col
+	end
+	word_start = word_start - 1 -- Convert to 0-indexed
+
+	local word_end = word_start + #word
 
 	local wrapped = char .. word .. char
 
@@ -16,7 +26,7 @@ function Wrapper.wrap_word(char)
 		wrapped = "{" .. word .. "}"
 	end
 
-	vim.api.nvim_buf_set_text(0, vim.fn.line(".") - 1, word_start, vim.fn.line(".") - 1, word_end, { wrapped })
+	vim.api.nvim_buf_set_text(0, row, word_start, row, word_end, { wrapped })
 end
 
 function Wrapper.setup()
@@ -25,7 +35,7 @@ function Wrapper.setup()
 	end, { noremap = true, silent = true, desc = "[W]rap word with single quotes" })
 
 	vim.keymap.set("n", '<leader>w"', function()
-		require("nvim_wrapper").wrap_word("\"")
+		require("nvim_wrapper").wrap_word('"')
 	end, { noremap = true, silent = true, desc = "[W]rap word with double quotes" })
 
 	vim.keymap.set("n", "<leader>w(", function()
