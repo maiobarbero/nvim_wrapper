bcba5d1db09cdfd3bfb689b9abe8d1ccd4a31217
Maio Barbero <maiowebdesign@gmail.com>
2025-03-31 09:53:55 +0200

    Fix: imporve brackets matching and fix keymapping

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local Wrapper 
+local Wrapper = {}
 
 function Wrapper.wrap_word(char)
 	local word = vim.fn.expand("<cword>")
@@ -6,46 +6,39 @@ function Wrapper.wrap_word(char)
 	local word_start = vim.fn.col("\\<") - 1
 	local word_end = vim.fn.col("\\>")
 
-    local wrapped = char .. word .. char
+	local wrapped = char .. word .. char
 
-    local par = char == "("
-    if par then
-        wrapped = "(" .. wrapped .. ")"
-    end
-
-    local square = char == "["
-    if square then
-        wrapped = "[" .. wrapped .. "]"
-    end
-
-    local curly = char == "{"
-    if curly then
-        wrapped = "{" .. wrapped .. "}"
-    end
+	if char == "(" then
+		wrapped = "(" .. word .. ")"
+	elseif char == "[" then
+		wrapped = "[" .. word .. "]"
+	elseif char == "{" then
+		wrapped = "{" .. word .. "}"
+	end
 
 	vim.api.nvim_buf_set_text(0, vim.fn.line(".") - 1, word_start, vim.fn.line(".") - 1, word_end, { wrapped })
 end
 
 function Wrapper.setup()
-	vim.keymap.set("n", "<leader>s'", function()
+	vim.keymap.set("n", "<leader>w'", function()
 		require("wrapper").wrap_word("'")
 	end, { noremap = true, silent = true, desc = "[W]rap word with single quotes" })
 
-	vim.keymap.set("n", "<leader>s\"", function()
-		require("wrapper").wrap_word("'")
-	end, { noremap = true, silent = true, desc = "[W]rap word with single quotes" })
+	vim.keymap.set("n", '<leader>w"', function()
+		require("wrapper").wrap_word("\"")
+	end, { noremap = true, silent = true, desc = "[W]rap word with double quotes" })
 
-	vim.keymap.set("n", "<leader>s(", function()
-		require("wrapper").wrap_word("'")
+	vim.keymap.set("n", "<leader>w(", function()
+		require("wrapper").wrap_word("(")
 	end, { noremap = true, silent = true, desc = "[W]rap word with parenthesis" })
 
-    vim.keymap.set("n", "<leader>s[", function()
-        require("wrapper").wrap_word("'")
-    end, { noremap = true, silent = true, desc = "[W]rap word with square brackets" })
+	vim.keymap.set("n", "<leader>w[", function()
+		require("wrapper").wrap_word("[")
+	end, { noremap = true, silent = true, desc = "[W]rap word with square brackets" })
 
-    vim.keymap.set("n", "<leader>s{", function()
-        require("wrapper").wrap_word("'")
-    end, { noremap = true, silent = true, desc = "[W]rap word with curly brackets" })
+	vim.keymap.set("n", "<leader>w{", function()
+		require("wrapper").wrap_word("{")
+	end, { noremap = true, silent = true, desc = "[W]rap word with curly brackets" })
 end
 
 return Wrapper
